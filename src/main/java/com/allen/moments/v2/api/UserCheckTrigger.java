3256c492b5f8c73e001e3a63508d370eb0cfc5e3
Manish Sharma <rsshrma14@hotmail.com>
2023-08-14 00:39:22 +0530

    Added the another controller

---
+++ src/main/java/com/allen/moments/v2/api/UserCheckTrigger.java
@@ -0,0 +1,14 @@
+package com.allen.moments.v2.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/userTriggered")
+public class UserCheckTrigger {
+
+    public void checkListOfRegisteredUser()
+    {
+
+    }
+}
